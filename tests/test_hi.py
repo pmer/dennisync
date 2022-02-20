222870aa7d5892fad26a03a92b1cd1201e349904
Paul Merrill <pdm@pdm.me>
2022-02-19 21:49:08 -0800

    format: tests

--- tests/test_hi.py
+++ tests/test_hi.py
@@ -1,8 +1,5 @@
-import pytest
-
 from dennisync import hi
 
 
-class TestHi:
-    def test_greet(self):
-        assert hi.greet() == "Hello, world!"
+def test_greet():
+    assert hi.greet() == "Hello, world!"
